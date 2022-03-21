37737a29d581ca68833b41483e2d7b5cc433334c
Alouette98 <36831940+Alouette98@users.noreply.github.com>
2022-03-21 03:42:41 -0400

    Update on Play1

--- Assets/Scripts/Level1/Play1/PlayOneManager.cs
+++ Assets/Scripts/Level1/Play1/PlayOneManager.cs
@@ -12,10 +12,14 @@ public class PlayOneManager : MonoBehaviour
     public Sprite GoSprite;
     public Sprite StopSprite;
 
+
+    public GameObject FullScreenCanvas;
+    public GameObject HalfCanvas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(FullScreenPop("Start",1.0f,true));
     }
 
     // Update is called once per frame
@@ -32,4 +36,25 @@ public class PlayOneManager : MonoBehaviour
             GoStopIndicatorObj.GetComponent<SpriteRenderer>().sprite = StopSprite;
         }
     }
+
+    IEnumerator FullScreenPop(string TextString, float PopUpTime,bool showHalf)
+    {
+        FullScreenCanvas.SetActive(true);
+        yield return new WaitForSeconds(PopUpTime);
+        FullScreenCanvas.SetActive(false);
+        yield return new WaitForSeconds(0.5f);
+        if (showHalf){
+            EnableHalf();
+        }
+    }
+
+    public void DisableHalf() {
+        HalfCanvas.SetActive(false);
+    }
+
+    public void EnableHalf()
+    {
+        HalfCanvas.SetActive(true);
+    }
+
 }
