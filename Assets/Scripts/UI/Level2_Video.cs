5e2d3e420dadf31cec2023f0431fba1da306b997
rohitsharma001 <43321039+rohitsharma001@users.noreply.github.com>
2022-04-20 23:14:57 -0400

    level 2 update

---
+++ Assets/Scripts/UI/Level2_Video.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+public class Level2_Video : MonoBehaviour
+{
+    //public string url;
+    VideoPlayer vidplayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vidplayer = GetComponent<VideoPlayer>();
+        //vidplayer.url = url;
+        vidplayer.url = System.IO.Path.Combine(Application.streamingAssetsPath, "intro.mp4");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Play();
+    }
+
+    void Play()
+    {
+        if (Input.anyKey)
+        {
+            vidplayer.Play();
+            vidplayer.isLooping = true;
+        }
+
+    }
+}
