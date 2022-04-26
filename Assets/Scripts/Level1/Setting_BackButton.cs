3a97d0f421b1ac08dde01a9c23e6811df568a9f0
rohitsharma001 <43321039+rohitsharma001@users.noreply.github.com>
2022-04-26 12:59:23 -0400

    home scene and level 2 changes

--- Assets/Scripts/Level1/Setting_BackButton.cs
+++ Assets/Scripts/Level1/Setting_BackButton.cs
@@ -2,54 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
 public class Setting_BackButton : MonoBehaviour
 {
-    /*
-
     public GameObject SettingsLayer;
     //public GameObject RoadDisplay;
-<<<<<<< Updated upstream
-
     //public GameManager gmr;
-
     public GameObject StartButton;
-=======
-    public GameObject settingscanvas;
-    //public GameManager gmr;
-    
->>>>>>> Stashed changes
-
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     public void CloseSettings()
     {
         // Show settings layer
         SettingsLayer.SetActive(false);
-        settingscanvas.SetActive(true);
         // Disable RoadDisplay
         //if (gmr.isTutorialFinished == true){
-<<<<<<< Updated upstream
         //    RoadDisplay.GetComponent<MeshRenderer>().enabled = true;
         //}
-
         StartButton.GetComponent<Button>().interactable = true;
-=======
-            //RoadDisplay.GetComponent<MeshRenderer>().enabled = true;
-            
-        //}
->>>>>>> Stashed changes
     }
-
     // Update is called once per frame
     void Update()
     {
         
     }
-
-    */
 }
