f2a56479c15237499aa515e17cc359f4c807c8e0
rohitsharma001 <43321039+rohitsharma001@users.noreply.github.com>
2022-05-10 02:04:32 -0400

    volume update

--- Assets/Scripts/Level1/SettingsButton.cs
+++ Assets/Scripts/Level1/SettingsButton.cs
@@ -6,9 +6,10 @@ using UnityEngine.UI;
 public class SettingsButton : MonoBehaviour
 {
 
-    public GameObject SettingsLayer;
+    public GameObject redMuteSign;
     //public GameObject RoadDisplay;
-    public GameObject StartButton;
+    //public GameObject StartButton;
+    private bool mute = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,13 +21,32 @@ public class SettingsButton : MonoBehaviour
     public void showSettings()
     {
         // Show settings layer
-        SettingsLayer.SetActive(true);
+
+
+        if(!mute)
+        {
+            redMuteSign.SetActive(true);
+            mute = true;
+            PlayerPrefs.SetFloat("volume", 0f);
+            AudioListener.volume = 0f;
+        }
+
+        else
+        {
+            mute = false;
+            redMuteSign.SetActive(false);
+            PlayerPrefs.SetFloat("volume", 0f);
+            AudioListener.volume = 1f;
+        }
+        
         // Disable RoadDisplay
         //RoadDisplay.GetComponent<MeshRenderer>().enabled = false;
 
-        StartButton.GetComponent<Button>().interactable = false;
+        //StartButton.GetComponent<Button>().interactable = false;
+
     }
 
+    
 
 
     // Update is called once per frame
